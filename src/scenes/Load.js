9e5578e4fc328e6f476f728da19ff474ac5a095c
Lane Albrecht <ltalbrec@ucsc.edu>
2021-04-22 15:31:29 -0700

    Initial Commit

---
+++ src/scenes/Load.js
@@ -0,0 +1,33 @@
+class Load extends Phaser.Scene {
+    constructor() {
+        super('loadScene');
+    }
+
+    preload() {
+        // to-do: loading bar
+
+        this.load.path = './assets/';
+        // load graphics assets
+        this.load.image('paddle', 'img/paddle.png');
+        this.load.image('fragment', 'img/fragment.png');
+        this.load.image('cross', 'img/white_cross.png');
+        // load audio assets
+        this.load.audio('beats', ['audio/beats.mp3']);
+        this.load.audio('clang', ['audio/clang.mp3']);
+        this.load.audio('death', ['audio/death.mp3']);
+        // load font
+        this.load.bitmapFont('gem', 'font/gem.png', 'font/gem.xml');
+    }
+
+    create() {
+        // check for local storage browser support
+        if(window.localStorage) {
+            console.log('Local storage supported');
+        } else {
+            console.log('Local storage not supported');
+        }
+
+        // go to Title scene
+        this.scene.start('titleScene');
+    }
+}
