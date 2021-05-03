4c15d1ed5b873b99f9f08f5293d495877428fc08
Joshua Huggins <jlhuggin@ucsc.edu>
2021-05-03 01:10:35 -0700

    Loading Bar and other Assets added

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,10 +4,10 @@ class Load extends Phaser.Scene {
     }
 
     preload() {
-        // to-do: loading bar
-
         this.load.path = './assets/';
         // load graphics assets
+        this.load.image('black','img/black.png')
+        this.load.image('GMG','img/gmg_logo.png')
         this.load.image('paddle', 'img/paddle.png');
         this.load.image('circle', 'img/circle.png');
         this.load.image('fragment', 'img/fragment.png');
@@ -18,11 +18,16 @@ class Load extends Phaser.Scene {
         this.load.audio('death', ['audio/death.mp3']);
         // load font
         this.load.bitmapFont('gem', 'font/gem.png', 'font/gem.xml');
-    }
 
-    create() {
-        this.loadingNum = 0;
-        let loadingNumConfig = {
+        // loading bar
+        
+        this.limg = this.add.image(centerX,centerY,'GMG').setOrigin(0.5);
+        
+            let width = this.cameras.main.width;
+            let height = this.cameras.main.height;
+
+            this.loadingNum = 0;
+            let loadingNumConfig = {
             fontFamily: 'Courier',
             fontSize: '48px',
             backgroundColor: '#F3B141',
@@ -34,29 +39,56 @@ class Load extends Phaser.Scene {
             },
             fixedWidth: 0
         }
-        this.ltext = this.add.text(centerX,centerY,'G M G', loadingNumConfig).setOrigin(0.5);
-        this.progBar = this.add.graphics();
-        this.progBox = this.add.graphics();
-        this.progBox.fillStyle(0xFFFFFF, 0.8);
-        this.progBox.fillRect(centerX - 125, centerY + 50, 250, 32);
+        //this.ltext = this.add.text(centerX,centerY,'G M G', loadingNumConfig).setOrigin(0.5);
+
+            let loadingText = this.make.text({
+                x: width / 2,
+                y: height / 2 + 185,
+                text: 'Loading...',
+                style: {
+                    font: '20px monospace',
+                    fill: '#ffffff'
+                }
+            });
+            loadingText.setOrigin(0.5, 0.5);
+            
+            let percentText = this.make.text({
+                x: width / 2,
+                y: height / 2 + 215,
+                text: '0%',
+                style: {
+                    font: '18px monospace',
+                    fill: '#ffffff'
+                }
+            });
+            percentText.setOrigin(0.5, 0.5);
+                         
+            this.load.on('progress', function (value) {
+                percentText.setText(parseInt(value * 100) + '%'); 
+            });
+            
+ 
+            this.load.on('complete', function () {
+                loadingText.destroy();
+                percentText.destroy();
+            });
+            
+            
+            for (let i = 0; i < 3000; i++) {
+                this.load.image('black', 'img/black.png')
+            }
+            
+
+
+
+        
     }
+
 
     update(){
-        if(this.loadingNum > 0 && this.loadingNum < 100){
-            this.updateLoadNum();
-        } else{
-            // go to Title scene
-            this.progBox.destroy();
-            this.progBar.destroy();
-            this.scene.start('titleScene');
+        if(true){
+        this.scene.start('titleScene');
         }
     }
-
-    updateLoadNum(){
-        ++this.loadingNum;
-        value = this.loadingNum;
-        this.progBar.fillStyle(0x000000, 1.0);
-        this.progBar.fillStyle(centerX - 125, centerY + 50, 240 * value, 22);
-        console.log(value);
-    }
+    
 }
